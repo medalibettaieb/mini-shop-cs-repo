df38b912ab52118a24d5c04f56c11920a447b765
bettaieb med ali <medali.bettaieb@esprit.tn>
2015-11-26 19:50:53 +0100

    list of categories + delete for next time do the update and find by name ... ;)
    Signed-off-by: bettaieb med ali <medali.bettaieb@esprit.tn>

--- mini-shop/mini-shop-web/src/main/java/tn/esprit/cs/gl/mini_shop/beans/CategoryBean.java
+++ mini-shop/mini-shop-web/src/main/java/tn/esprit/cs/gl/mini_shop/beans/CategoryBean.java
@@ -1,16 +1,21 @@
 package tn.esprit.cs.gl.mini_shop.beans;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
 
 import tn.esprit.cs.gl.mini_shop.persistence.Category;
 import tn.esprit.cs.gl.mini_shop.services.CatalogServiceLocal;
 
 @ManagedBean
-@RequestScoped
+@ViewScoped
 public class CategoryBean {
 	private Category category = new Category();
+	private List<Category> categories = new ArrayList<>();
+	private Category categoryChosen = new Category();
 
 	@EJB
 	private CatalogServiceLocal catalogServiceLocal;
@@ -20,12 +25,34 @@ public class CategoryBean {
 		return "";
 	}
 
+	public String dodeleteCategory() {
+		catalogServiceLocal.removeCategory(categoryChosen);
+		return "";
+	}
+
 	public Category getCategory() {
 		return category;
 	}
 
 	public void setCategory(Category category) {
 		this.category = category;
+	}
+
+	public List<Category> getCategories() {
+		categories = catalogServiceLocal.findAllCategories();
+		return categories;
+	}
+
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
+	}
+
+	public Category getCategoryChosen() {
+		return categoryChosen;
+	}
+
+	public void setCategoryChosen(Category categoryChosen) {
+		this.categoryChosen = categoryChosen;
 	}
 
 }
